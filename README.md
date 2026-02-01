fd052983008aa9e610929380ae05b3cd6338351c
Switch <25269380+Switch123456789@users.noreply.github.com>
2026-02-01 06:23:34 +0100

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Bash-Scripts
-Some Bash-Scripts that do things.
+### About:
+A collection of scripts that do things. They are TUI-based using "whiptail".
+### Infos:
+Make scripts executable if needed with: `chmod +x *`.  I assume these scripts should work on most linux-setups, **only `whiptail`** might be something that is not yet installed on your system.
+
+# ffs.sh (FreeFileSync)
+## Create modified config to remove excluded files:
+This is meant for easy removal of files that were not deleted on the right side before excluding them in FFS (When mirroring from left to right). They can be hard to spot if a lot of changes have been made to the exclusion list. This also works with multiple pairs and separate pair-settings.
+
+### Usage:
+1. In `scripts.cfg` **set `FFS_PATH_BIN`** accordingly.
+2. **Start script** with `./ffs.sh "/path/to/config_file.ffs_gui"`, this will open FFS with a modified .ffs_gui file.
+5. **Click `Compare`** and all leftover files should show up.
+6. **Quadruple-check** if those files can be removed safely (It works perfectly for me, but I didn't do a whole lot of testing).
+7. **Click `Synchronize`** to remove them.
+8. If you **close FFS** now, the script will remove the modified config-file again and is done.
+
+### How does it work:
+It creates a modified version of the source-.ffs_gui-file, by replacing the left side with an empty folder and setting all exclusions to be inclusions. This creates a scenario in which FFS mirrors "nothing" to all originally excluded files and therefore removing them on the right side when executing the Synchronization.
---
+++ ffs.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+	source "$(pwd)/scripts.lib" || exit
+
+#[LOAD CONFIG]
+	SCRIPT_NAME="ffs"
+	SCRIPT_JOB="Launch with config that removes exclusions in destination-folder"
+	if [[ -z "${FFS_PATH_BIN:-}" ]]; then tui_info "FFS_PATH_BIN missing in ffs.cfg"; exit; fi
+
+#[CREATE MODIFIED CONFIG]
+	# Create an empty folder that can be mirrored to the destination
+	rm -rf "${SCRIPT_PATH:?}/.tmp/*"; mkdir -p "${SCRIPT_PATH}/.tmp/null"; cd "${SCRIPT_PATH}/.tmp" || exit 1
+	SKIP=false
+	# Iterate through input file
+	{ while IFS= read -r LINE; do
+		# SKIPPING LINES START
+		case "${LINE//$'\r'/}" in (*"<Include>"|*"<Synchronize>")
+				SKIP=true
+		;;
+		# SKIPPING LINES STOP
+		(*"</Include>"|"            </Synchronize>")
+				SKIP=false
+		;;
+		# GLOBAL: INJECT SYNC CONFIG
+		("    </Synchronize>")
+				SKIP=false
+				printf '%s\n' \
+				'    <Synchronize>' \
+				'        <Differences LeftOnly="right" LeftNewer="right" RightNewer="right" RightOnly="right"/>' \
+				'        <DeletionPolicy>Permanent</DeletionPolicy>' \
+				'        <VersioningFolder Style="Replace"/>' \
+				'    </Synchronize>'
+		;;
+		# GLOBAL: INJECT LEFT PATH 
+		("            <Left>"*"</Left>")
+				printf '%s\n' "            <Left>${SCRIPT_PATH}/.tmp/null</Left>"
+		;;
+		# GLOBAL: REVERSE FILTER
+		("        <Exclude>")
+				printf '%s\n' '        <Include>' '<Item>*</Item>'
+		;;
+		("        </Exclude>")
+				printf '%s\n' '        </Include>' '        <Exclude/>'
+		;;
+		("        <Exclude/>")
+				printf '%s\n' '        <Include/>' '        <Exclude/>'
+		;;
+		# PAIR: REVERSE FILTER
+		("                <Exclude>")
+				printf '%s\n' '                <Include>'
+		;;
+		("                </Exclude>")
+				printf '%s\n' '                </Include>' '                <Exclude/>'
+		;;
+		("                <Exclude/>")
+				printf '%s\n' '                <Include/>' '                <Exclude/>'
+		;;
+		# PASS-THROUGH
+		(*)
+			if [ "${SKIP}" = false ]; then
+						printf '%s\n' "$LINE"
+			fi
+		;; esac
+	done < "${1}"; } > "${SCRIPT_PATH}/.tmp/TEMPORARY-CONFIG.ffs_gui";
+		
+#[OPEN CONFIG WITH FFS & CLEANUP AFTER CLOSING IT]
+	thread() { ${FFS_PATH_BIN} "${SCRIPT_PATH}/.tmp/TEMPORARY-CONFIG.ffs_gui" -Edit; }
+	thread_single "Launching FreeFileSync"
+	rm -rf "${SCRIPT_PATH}/.tmp"
